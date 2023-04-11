e6d23e440957307d3fab1693197cc29f00e66d59
Official0mega <supr3m30m3ga@gmail.com>
2023-04-11 22:51:51 +0100

    Loops

---
+++ middle/infinite.py
@@ -0,0 +1,11 @@
+#!/use/bin/python
+
+infix = 1
+
+while (infix == 1):
+    loopted = input("Enter a number :")
+    print("You Entered: ", loopted)
+
+
+
+print("  End of Loops  ")
---
+++ middle/while.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+flow = 0
+
+while (flow < 20):
+    print('This is flow number: ', flow)
+    flow = flow + 2
+
+
+print("The Last Comment")
