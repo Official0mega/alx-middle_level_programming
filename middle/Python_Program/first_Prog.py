290774adf335de8b813a2c51df97b3ac2501a68e
Official0mega <supr3m30m3ga@gmail.com>
2023-04-18 14:45:55 +0100

    Python Calculator

---
+++ middle/Python_Program/first_Prog.py
@@ -0,0 +1,9 @@
+# code
+print("First Python Program Created by Official0mega(Crazy_Hacker)!")
+
+num = int(input())
+
+if num%2 == 0:
+    print("It's an Even number!")
+else:
+    print("It's an Odd number!")
