f2db5f11d9345d4ffaca755d46138927e6f4d52a
Official0mega <supr3m30m3ga@gmail.com>
2023-04-21 10:39:01 +0100

    Second initial commit

---
+++ high/second.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+name = input("Enter your Name: ")
+
+print("Hello " + name)
+
+print(' \nString Manipulation Exercises\n String Concatenation is done with "+" Sign\n e.g print("Hello" + "Jenny")\n New lines can be created with a backslash and n\n')
+
+hat_file = input("What is Thy Name 🤔: ")
+print("Hey " + hat_file + " How are You?")
