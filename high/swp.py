16ddef76807d58b36a71b369745bee42a09d5964
Official0mega <supr3m30m3ga@gmail.com>
2023-04-21 16:10:31 +0100

    variables intake

---
+++ high/swp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+right = input("Enter a Number: ")
+left = input("Enter another Number: ")
+
+middle = right
+right = left
+left = middle
+
+print("right = " + right)
+print("left = " + left)
+
